dfa03fdab9dc98b824ea3b98abc3db0a3719e225
futakotome <v-zzz.gd@chinatelecom.cn>
2022-02-06 14:17:43 +0800

    sth

--- src/router/routes/index.ts
+++ src/router/routes/index.ts
@@ -2,6 +2,18 @@ import type {RouteRecordRaw} from "vue-router";
 import {PageEnum} from "../../enum/pageEnum";
 import {PAGE_NOT_FOUNT_ROUTE, REDIRECT_ROUTE} from "./basic";
 
+const modules = import.meta.globEager('./modules/**/*.ts');
+
+const routeModuleList: RouteRecordRaw[] = [];
+
+Object.keys(modules).forEach((key) => {
+    const mod = modules[key].default || {};
+    const modList = Array.isArray(mod) ? [...mod] : [mod];
+    routeModuleList.push(...modList);
+})
+
+export const asyncRoutes = [PAGE_NOT_FOUNT_ROUTE, ...routeModuleList];
+
 export const RootRoute: RouteRecordRaw = {
     path: '/',
     name: 'Root',
