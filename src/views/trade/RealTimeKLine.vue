0a456d408b660fe1e90aa974fdda5eca7a11672d
futakotome <v-zzz.gd@chinatelecom.cn>
2023-04-25 22:28:12 +0800

    more try

--- src/views/trade/RealTimeKLine.vue
+++ src/views/trade/RealTimeKLine.vue
@@ -1,70 +1,135 @@
 <script setup lang="ts">
-import { RSocketClient, JsonSerializer, IdentitySerializer } from 'rsocket-core';
-import RSocketWebSocketClient from 'rsocket-websocket-client';
-import FlowableProcessor from 'rsocket-flowable'
-import { ref } from 'vue';
+import {
+    type OnExtensionSubscriber,
+    type OnNextSubscriber,
+    type OnTerminalSubscriber,
+    type Payload,
+    RSocketConnector,
+    RSocketServer,
+} from 'rsocket-core'
+import { WebsocketClientTransport } from 'rsocket-websocket-client'
+import { WebsocketServerTransport } from 'rsocket-websocket-server'
+import WebSocket from 'isomorphic-ws'
 
-const result = ref<string>("")
+function makeServer() {
+    return new RSocketServer({
+        transport: new WebsocketServerTransport({
+            host: "localhost",
+            port: 9000
+        }),
+        acceptor: {
+            accept: async () => {
+                return {
+                    requestStream: (payload: Payload,
+                        initialRequestN,
+                        responderStream: OnTerminalSubscriber & OnNextSubscriber & OnExtensionSubscriber) => {
+                        console.log(
+                            `[server] requestStream payload[data: ${payload.data}; metadata: ${payload.metadata}]|initialRequestN: ${initialRequestN}`
+                        );
 
-const client = new RSocketClient({
-    serializers: {
-        data: JsonSerializer,
-        metadata: IdentitySerializer
-    },
-    setup: {
-        keepAlive: 60000,
-        lifetime: 180000,
-        dataMimeType: 'application/json',
-        metadataMimeType: 'message/x.rsocket.routing.v0',
-    },
-    transport: new RSocketWebSocketClient({
-        url: "ws://localhost:9000/rsocket"
-    })
-})
+                        let interval: any = null;
+                        let requestedResponse = initialRequestN;
+                        let sentResponse = 0;
 
-const errorHanlder = (e) => console.log(e)
+                        interval = setInterval(() => {
+                            sentResponse++;
+                            let isComplete = sentResponse >= requestedResponse;
+                            responderStream.onNext(
+                                {
+                                    data: Buffer.from(new Date()),
+                                    metadata: undefined
+                                },
+                                isComplete
+                            );
+                            if (isComplete) {
+                                clearInterval(interval)
+                            }
+                        }, 750)
 
-const responseHanlder = (payload) => {
-    console.log(payload)
-    result.value = payload.data
-}
-
-const processor = new FlowableProcessor(sub => {
-    console.log(sub)
-})
-
-
-const numberRequester = (socket, processor) => {
-    console.log(processor)
-    socket.requestChannel(processor.map(i => {
-        return {
-            data: i,
-            metadata: String.fromCharCode('number.channel'.length) + 'number.channel'
+                        return {
+                            cancel() {
+                                console.log("[server] stream cancelled by client");
+                                clearInterval(interval)
+                            },
+                            request(n) {
+                                requestedResponse += n;
+                                console.log(
+                                    `[server] request n: ${n}, requestedResponses: ${requestedResponse}, sentResponses: ${sentResponse}`)
+                            },
+                            onExtension: () => { }
+                        }
+                    }
+                }
+            }
         }
-    })).subscribe({
-        onError: errorHanlder,
-        onNext: responseHanlder,
-        onSubscribe: subscription => {
-            subscription.request(100); // set it to some max value
-        }
     })
-
 }
 
-const n = ref(null)
-
-client.connect().then(socket => {
-    numberRequester(socket, processor);
-}, errorHanlder)
-
-function publish() {
-    processor.onNext(parseInt('1'))
+function makeConnector() {
+    return new RSocketConnector({
+        transport: new WebsocketClientTransport({
+            url: "ws://localhost:9000"
+        })
+    })
 }
+
+let serverCloseable: any;
+
+async function main() {
+    const server = makeServer();
+    const connector = makeConnector();
+
+    serverCloseable = await server.bind();
+    const rsocket = await connector.connect();
+
+    await new Promise((resolve, reject) => {
+        let payloadsReceived = 0;
+        const maxPayloads = 10;
+        const requester = rsocket.requestStream({
+            data: Buffer.from("hello world")
+        },
+            3,
+            {
+                onError: (e) => console.log(e),
+                onNext: (payload, isComplete) => {
+                    console.log(`[client] payload[data: ${payload.data}; metadata: ${payload.metadata}]|isComplete: ${isComplete}`)
+                    payloadsReceived++;
+                    // request 5 more payloads every 5th payload, until a max total payloads received
+                    if (payloadsReceived % 2 == 0 && payloadsReceived < maxPayloads) {
+                        requester.request(2);
+                    } else if (payloadsReceived >= maxPayloads) {
+                        requester.cancel();
+                        setTimeout(() => {
+                            resolve(null);
+                        });
+                    }
+
+                    if (isComplete) {
+                        resolve(null);
+                    }
+                },
+                onComplete: () => {
+                    resolve(null);
+                },
+                onExtension: () => { }
+            }
+        )
+    })
+}
+
+main()
+    .then(() => { })
+    .catch((error: Error) => {
+        console.error(error);
+    })
+    .finally(() => {
+        serverCloseable.close();
+    });
+
 </script>
 <template>
     <div>实时K
-        <input type="text" @keyup="publish">
-        <div>RSocket推送结果 : {{ result }}</div>
+
     </div>
 </template>
 <style lang="less" scoped></style>
