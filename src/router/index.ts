4fe87170b8b516e00faafc38645266e1f5708bc2
futakotome <983528987@qq.com>
2025-02-05 16:48:38 +0800

    优化整合第三方ft模块页面

--- src/router/index.ts
+++ src/router/index.ts
@@ -42,65 +42,188 @@ const routes: RouteRecordRaw[] = [
     }]
   },
   {
-    path: "/market",
-    name: "MartetPage",
-    redirect: "/market/stockList",
+    path: "/thirdFutu",
+    name: "ThirdFutuPage",
+    redirect: "/thirdFutu/market",
     meta: {
-      title: "市场",
+      title: "第三方-富途",
       icon: "account-book-filled"
     },
     component: () => import("@/layouts/Layout.vue"),
     children: [
       {
-        path: "/market/stockList",
-        name: "StockList",
+        path: "/thirdFutu/market",
+        name: "MartetPage",
+        redirect: "/thirdFutu/market/stockList",
         meta: {
-          title: "正股",
-          icon: "container-filled"
+          title: "市场",
+          icon: "account-book-filled"
         },
-        component: () => import("@/views/market/StockList.vue")
+        children: [
+          {
+            path: "/thirdFutu/market/stockList",
+            name: "StockList",
+            meta: {
+              title: "正股",
+              icon: "container-filled"
+            },
+            component: () => import("@/views/market/StockList.vue")
+          },
+          {
+            path: "/thirdFutu/market/futuresList",
+            name: "FuturesList",
+            meta: {
+              title: "期货",
+              icon: "bank-filled"
+            },
+            component: () => import("@/views/market/FuturesList.vue")
+          },
+          {
+            path: "/thirdFutu/market/indies",
+            name: "IndexList",
+            meta: {
+              title: "指数",
+              icon: "line-chart-outlined"
+            },
+            component: () => import("@/views/market/Indies.vue")
+          }, {
+            path: "/thirdFutu/market/bond",
+            name: "BondList",
+            meta: {
+              title: "债券",
+              icon: "audit-outlined"
+            },
+            component: () => import("@/views/market/Bond.vue")
+          }, {
+            path: "/thirdFutu/market/warrant",
+            name: "WarrantList",
+            meta: {
+              title: "窝轮",
+              icon: "chrome-outlined"
+            },
+            component: () => import("@/views/market/Warrant.vue")
+          }, {
+            path: "/thirdFutu/market/plates",
+            name: "PlateList",
+            meta: {
+              title: "板块",
+              icon: "pie-chart-outlined"
+            },
+            component: () => import("@/views/market/PlateList.vue")
+          }
+        ]
       },
       {
-        path: "/market/futuresList",
-        name: "FuturesList",
+        path: "/thirdFutu/trade",
+        name: "TradePage",
+        redirect: "/thirdFutu/trade/account",
         meta: {
-          title: "期货",
+          title: "交易",
           icon: "bank-filled"
         },
-        component: () => import("@/views/market/FuturesList.vue")
+        children: [
+          {
+            path: "/thirdFutu/trade/account",
+            name: "AccountPage",
+            meta: {
+              title: "账户管理",
+              icon: "credit-card-outlined"
+            },
+            component: () => import("@/views/trade/AccountPage.vue")
+          },
+          {
+            path: "/thirdFutu/trade/position",
+            name: "PositionPage",
+            meta: {
+              title: "持仓管理",
+              icon: "table-outlined"
+            },
+            component: () => import("@/views/trade/PositionPage.vue")
+          },
+          {
+            path: "/thirdFutu/trade/sub",
+            name: "SubscribePage",
+            meta: {
+              title: "富途订阅",
+              icon: "bank-filled"
+            },
+            component: () => import("@/views/trade/SubscribePage.vue")
+          }
+        ]
       },
       {
-        path: "/market/indies",
-        name: "IndexList",
+        path: "/thirdFutu/realtime",
+        name: "RTMonitor",
+        redirect: "/thirdFutu/realtime/rtk_min1",
         meta: {
-          title: "指数",
-          icon: "line-chart-outlined"
+          title: "实时",
+          icon: "fund-outlined"
         },
-        component: () => import("@/views/market/Indies.vue")
-      }, {
-        path: "/market/bond",
-        name: "BondList",
-        meta: {
-          title: "债券",
-          icon: "audit-outlined"
-        },
-        component: () => import("@/views/market/Bond.vue")
-      }, {
-        path: "/market/warrant",
-        name: "WarrantList",
-        meta: {
-          title: "窝轮",
-          icon: "chrome-outlined"
-        },
-        component: () => import("@/views/market/Warrant.vue")
-      }, {
-        path: "/market/plates",
-        name: "PlateList",
-        meta: {
-          title: "板块",
-          icon: "pie-chart-outlined"
-        },
-        component: () => import("@/views/market/PlateList.vue")
+        children: [
+          {
+            path: "/thirdFutu/realtime/rtk_min1",
+            name: "RealTimeKMin1",
+            meta: {
+              title: "1分钟实时K线图",
+              icon: "sliders-outlined"
+            },
+            component: () => import("@/views/trade/k/RealTimeKLine.vue")
+          },
+          {
+            path: "/thirdFutu/realtime/rtk_min3",
+            name: "RealTimeKMin3",
+            meta: {
+              title: "3分钟实时K线图",
+              icon: "sliders-outlined"
+            },
+            component: () => import("@/views/trade/k/RealTimeKLineMin3.vue")
+          },
+          {
+            path: "/thirdFutu/realtime/rtk_min5",
+            name: "RealTimeKMin5",
+            meta: {
+              title: "5分钟实时K线图",
+              icon: "sliders-outlined"
+            },
+            component: () => import("@/views/trade/k/RealTimeKLineMin5.vue")
+          },
+          {
+            path: "/thirdFutu/realtime/rtk_min15",
+            name: "RealTimeKMin15",
+            meta: {
+              title: "15分钟实时K线图",
+              icon: "sliders-outlined"
+            },
+            component: () => import("@/views/trade/k/RealTimeKLineMin15.vue")
+          },
+          {
+            path: "/thirdFutu/realtime/rtk_min30",
+            name: "RealTimeKMin30",
+            meta: {
+              title: "30分钟实时K线图",
+              icon: "sliders-outlined"
+            },
+            component: () => import("@/views/trade/k/RealTimeKLineMin30.vue")
+          },
+          {
+            path: "/thirdFutu/realtime/rtk_min60",
+            name: "RealTimeKMin60",
+            meta: {
+              title: "60分钟实时K线图",
+              icon: "sliders-outlined"
+            },
+            component: () => import("@/views/trade/k/RealTimeKLineMin60.vue")
+          },
+          {
+            path: "/thirdFutu/realtime/rto",
+            name: "RealTimeBaseQuote",
+            meta: {
+              title: "实时报价",
+              icon: "area-chart-outlined"
+            },
+            component: () => import("@/views/trade/RealTimeBaseQuote.vue")
+          }
+        ]
       }
     ]
   },
@@ -130,119 +253,6 @@ const routes: RouteRecordRaw[] = [
           icon: "bg-colors-outlined"
         },
         component: () => import("@/views/operation/DataCleaning.vue")
-      }
-    ]
-  },
-  {
-    path: "/trade",
-    name: "TradePage",
-    redirect: "/trade/rtk",
-    meta: {
-      title: "交易",
-      icon: "bank-filled"
-    },
-    component: () => import("@/layouts/Layout.vue"),
-    children: [
-      {
-        path: "/trade/account",
-        name: "AccountPage",
-        meta: {
-          title: "账户管理",
-          icon: "credit-card-outlined"
-        },
-        component: () => import("@/views/trade/AccountPage.vue")
-      },
-      {
-        path: "/trade/position",
-        name: "PositionPage",
-        meta: {
-          title: "持仓管理",
-          icon: "table-outlined"
-        },
-        component: () => import("@/views/trade/PositionPage.vue")
-      },
-      {
-        path: "/trade/sub",
-        name: "SubscribePage",
-        meta: {
-          title: "富途订阅",
-          icon: "bank-filled"
-        },
-        component: () => import("@/views/trade/SubscribePage.vue")
-      }
-    ]
-  },
-  {
-    path: "/realtime",
-    name: "RTMonitor",
-    meta: {
-      title: "实时",
-      icon: "fund-outlined"
-    },
-    component: () => import("@/layouts/Layout.vue"),
-    children: [
-      {
-        path: "/realtime/rtk_min1",
-        name: "RealTimeKMin1",
-        meta: {
-          title: "1分钟实时K线图",
-          icon: "sliders-outlined"
-        },
-        component: () => import("@/views/trade/k/RealTimeKLine.vue")
-      },
-      {
-        path: "/realtime/rtk_min3",
-        name: "RealTimeKMin3",
-        meta: {
-          title: "3分钟实时K线图",
-          icon: "sliders-outlined"
-        },
-        component: () => import("@/views/trade/k/RealTimeKLineMin3.vue")
-      },
-      {
-        path: "/realtime/rtk_min5",
-        name: "RealTimeKMin5",
-        meta: {
-          title: "5分钟实时K线图",
-          icon: "sliders-outlined"
-        },
-        component: () => import("@/views/trade/k/RealTimeKLineMin5.vue")
-      },
-      {
-        path: "/realtime/rtk_min15",
-        name: "RealTimeKMin15",
-        meta: {
-          title: "15分钟实时K线图",
-          icon: "sliders-outlined"
-        },
-        component: () => import("@/views/trade/k/RealTimeKLineMin15.vue")
-      },
-      {
-        path: "/realtime/rtk_min30",
-        name: "RealTimeKMin30",
-        meta: {
-          title: "30分钟实时K线图",
-          icon: "sliders-outlined"
-        },
-        component: () => import("@/views/trade/k/RealTimeKLineMin30.vue")
-      },
-      {
-        path: "/realtime/rtk_min60",
-        name: "RealTimeKMin60",
-        meta: {
-          title: "60分钟实时K线图",
-          icon: "sliders-outlined"
-        },
-        component: () => import("@/views/trade/k/RealTimeKLineMin60.vue")
-      },
-      {
-        path: "/realtime/rto",
-        name: "RealTimeBaseQuote",
-        meta: {
-          title: "实时报价",
-          icon: "area-chart-outlined"
-        },
-        component: () => import("@/views/trade/RealTimeBaseQuote.vue")
       }
     ]
   },
