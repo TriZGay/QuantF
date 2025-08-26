fc5eceef65da4cca0cc8bdf026b32d43546c9ecb
futakotome <983528987@qq.com>
2025-08-26 16:59:49 +0800

    echart升级到6

--- src/main.ts
+++ src/main.ts
@@ -5,20 +5,15 @@ import App from "./App.vue";
 import { router } from "@/router";
 
 import "ant-design-vue/dist/antd.css";
-import * as Icons from '@ant-design/icons-vue'
+import * as Icons from "@ant-design/icons-vue";
 
 const app = createApp(App);
 
-import 'echarts';
-import VChart from 'vue-echarts';
-
-app.component("VChart", VChart);
-
 for (const i in Icons) {
-    //@ts-ignore
-    app.component(i, Icons[i])
+  //@ts-ignore
+  app.component(i, Icons[i]);
 }
-app.config.globalProperties.$antIcons = Icons
+app.config.globalProperties.$antIcons = Icons;
 
 app.use(createPinia());
 app.use(router);
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,8 +1,9 @@
-<script setup>
-
+<script setup lang="ts">
+import TradingPane from "@/components/TradingPane/index.vue";
 </script>
 <template>
   <div>首页
+    <TradingPane />
   </div>
 </template>
 <style scoped>
